2f45d2f6f8e2fd87c52d1f0f5583789ceaf3276f
octavonce <octavian.oncescu@gmail.com>
2018-11-15 11:39:29 +0200

    Added mint tx serialization logic

--- src/transactions/src/mint.rs
+++ src/transactions/src/mint.rs
@@ -17,6 +17,7 @@
 */
 
 use account::{Address, Balance, Signature};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use crypto::Hash;
 use serde::{Deserialize, Serialize};
 use transaction::*;
@@ -32,4 +33,62 @@ pub struct Mint {
     hash: Option<Hash>,
     #[serde(skip_serializing_if = "Option::is_none")]
     signature: Option<Signature>,
+}
+
+impl Mint {
+    /// Serializes the transaction struct to a binary format.
+    ///
+    /// Fields:
+    /// 1) Transaction type(10)     - 8bits
+    /// 2) Fee length               - 8bits
+    /// 3) Amount length            - 8bits
+    /// 4) Signature length         - 16bits
+    /// 5) Minter                   - 32byte binary
+    /// 6) Currency hash            - 32byte binary
+    /// 7) Fee hash                 - 32byte binary
+    /// 8) Hash                     - 32byte binary
+    /// 9) Amount                   - Binary of amount length
+    /// 10) Fee                     - Binary of fee length
+    /// 11) Signature               - Binary of signature length
+    pub fn to_bytes(&self) -> Result<Vec<u8>, &'static str> {
+        let mut buffer: Vec<u8> = Vec::new();
+        let tx_type: u8 = 10;
+
+        let hash = if let Some(hash) = &self.hash {
+            &hash.0
+        } else {
+            return Err("Hash field is missing");
+        };
+
+        let mut signature = if let Some(signature) = &self.signature {
+            signature.to_bytes()
+        } else {
+            return Err("Signature field is missing");
+        };
+
+        let minter = &self.minter.to_bytes();
+        let currency_hash = &&self.currency_hash.0;
+        let fee_hash = &&self.fee_hash.0;
+        let amount = &self.amount.to_bytes();
+        let fee = &self.fee.to_bytes();
+
+        let fee_len = fee.len();
+        let amount_len = amount.len();
+        let signature_len = signature.len();
+
+        buffer.write_u8(tx_type).unwrap();
+        buffer.write_u8(fee_len as u8).unwrap();
+        buffer.write_u8(amount_len as u8).unwrap();
+        buffer.write_u16::<BigEndian>(signature_len as u16).unwrap();
+
+        buffer.append(&mut minter.to_vec());
+        buffer.append(&mut currency_hash.to_vec());
+        buffer.append(&mut fee_hash.to_vec());
+        buffer.append(&mut hash.to_vec());
+        buffer.append(&mut amount.to_vec());
+        buffer.append(&mut fee.to_vec());
+        buffer.append(&mut signature);
+
+        Ok(buffer)
+    }
 }
