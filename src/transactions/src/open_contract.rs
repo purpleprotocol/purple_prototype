a88d389f8ba7121f178c87a6c7f1ef2ef73e2a4a
octavonce <octavian.oncescu@gmail.com>
2018-11-15 11:48:36 +0200

    Added open contract tx serialization logic

--- src/transactions/src/open_contract.rs
+++ src/transactions/src/open_contract.rs
@@ -17,6 +17,7 @@
 */
 
 use account::{Address, Balance, Signature};
+use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
 use crypto::Hash;
 use serde::{Deserialize, Serialize};
 use transaction::*;
@@ -32,4 +33,62 @@ pub struct OpenContract {
     hash: Option<Hash>,
     #[serde(skip_serializing_if = "Option::is_none")]
     signature: Option<Signature>,
+}
+
+impl OpenContract {
+    /// Serializes the transaction struct to a binary format.
+    ///
+    /// Fields:
+    /// 1) Transaction type(2)      - 8bits
+    /// 2) Fee length               - 8bits
+    /// 3) State length             - 16bits
+    /// 4) Code length              - 16bits
+    /// 5) Owner                    - 32byte binary
+    /// 6) Fee hash                 - 32byte binary
+    /// 8) Hash                     - 32byte binary
+    /// 9) Signature                - 64byte binary
+    /// 10) Fee                     - Binary of fee length
+    /// 11) Default state           - Binary of state length
+    /// 12) Code                    - Binary of code length
+    pub fn to_bytes(&self) -> Result<Vec<u8>, &'static str> {
+        let mut buffer: Vec<u8> = Vec::new();
+        let tx_type: u8 = 2;
+
+        let hash = if let Some(hash) = &self.hash {
+            &hash.0
+        } else {
+            return Err("Hash field is missing");
+        };
+
+        let mut signature = if let Some(signature) = &self.signature {
+            signature.to_bytes()
+        } else {
+            return Err("Signature field is missing");
+        };
+
+        let owner = &self.owner.to_bytes();
+        let fee_hash = &&self.fee_hash.0;
+        let code = &self.code.as_bytes();
+        let default_state = &self.default_state.as_bytes();
+        let fee = &self.fee.to_bytes();
+
+        let fee_len = fee.len();
+        let code_len = code.len();
+        let state_len = default_state.len();
+
+        buffer.write_u8(tx_type).unwrap();
+        buffer.write_u8(fee_len as u8).unwrap();
+        buffer.write_u16::<BigEndian>(state_len as u16).unwrap();
+        buffer.write_u16::<BigEndian>(code_len as u16).unwrap();
+
+        buffer.append(&mut owner.to_vec());
+        buffer.append(&mut fee_hash.to_vec());
+        buffer.append(&mut hash.to_vec());
+        buffer.append(&mut signature.to_vec());
+        buffer.append(&mut fee.to_vec());
+        buffer.append(&mut default_state.to_vec());
+        buffer.append(&mut code.to_vec());
+
+        Ok(buffer)
+    }
 }
