f5f3c638854dea79b20627fe47fae3857f5dbebc
octavonce <octavian.oncescu@gmail.com>
2018-12-02 17:18:27 +0200

    Added OpenContract sign and hash logic

--- src/transactions/src/open_contract.rs
+++ src/transactions/src/open_contract.rs
@@ -16,9 +16,9 @@
   along with the Purple Library. If not, see <http://www.gnu.org/licenses/>.
 */
 
-use account::{Address, Balance, Signature};
+use account::{Address, Balance, Signature, ShareMap, MultiSig};
 use byteorder::{BigEndian, ReadBytesExt, WriteBytesExt};
-use crypto::Hash;
+use crypto::{Hash, SecretKey as Sk, PublicKey as Pk};
 use serde::{Deserialize, Serialize};
 use transaction::*;
 use std::str;
@@ -39,6 +39,126 @@ pub struct OpenContract {
 
 impl OpenContract {
     pub const TX_TYPE: u8 = 2;
+
+    /// Signs the transaction with the given secret key.
+    ///
+    /// This function will panic if there already exists
+    /// a signature and the address type doesn't match
+    /// the signature type.
+    pub fn sign(&mut self, skey: Sk) {
+        // Assemble data
+        let message = assemble_sign_message(&self);
+
+        // Sign data
+        let signature = crypto::sign(&message, skey);
+
+        match self.signature {
+            Some(Signature::Normal(_)) => { 
+                if let Address::Normal(_) = self.owner {
+                    let result = Signature::Normal(signature);
+                    self.signature = Some(result);
+                } else {
+                    panic!("Invalid address type");
+                }
+            },
+            Some(Signature::MultiSig(ref mut sig)) => {
+                if let Address::Normal(_) = self.owner {
+                    panic!("Invalid address type");
+                } else {
+                    // Append signature to the multi sig struct
+                    sig.append_sig(signature);
+                }           
+            },
+            None => {
+                if let Address::Normal(_) = self.owner {
+                    // Create a normal signature
+                    let result = Signature::Normal(signature);
+                    
+                    // Attach signature to struct
+                    self.signature = Some(result);
+                } else {
+                    // Create a multi signature
+                    let result = Signature::MultiSig(MultiSig::from_sig(signature));
+
+                    // Attach signature to struct
+                    self.signature = Some(result);
+                }
+            }
+        };
+    }
+    
+    /// Verifies the signature of the transaction.
+    ///
+    /// Returns `false` if the signature field is missing.
+    ///
+    /// This function panics if the transaction has a multi 
+    /// signature attached to it or if the signer's address
+    /// is not a normal address.
+    pub fn verify_sig(&mut self) -> bool {
+        let message = assemble_sign_message(&self);
+
+        match self.signature {
+            Some(Signature::Normal(ref sig)) => { 
+                if let Address::Normal(ref addr) = self.owner {
+                    crypto::verify(&message, sig.clone(), addr.pkey())
+                } else {
+                    panic!("The address of the signer is not a normal address!");
+                }
+            },
+            Some(Signature::MultiSig(_)) => {
+                panic!("Calling this function on a multi signature transaction is not permitted!");
+            },
+            None => {
+                false
+            }
+        }
+    }
+
+    /// Verifies the multi signature of the transaction.
+    ///
+    /// Returns `false` if the signature field is missing.
+    ///
+    /// This function panics if the transaction has a multi 
+    /// signature attached to it or if the signer's address
+    /// is not a normal address.
+    pub fn verify_multi_sig(&mut self, required_keys: u8, pkeys: &[Pk]) -> bool {
+        if pkeys.len() < required_keys as usize {
+            false
+        } else {
+            let message = assemble_sign_message(&self);
+
+            match self.signature {
+                Some(Signature::Normal(_)) => { 
+                    panic!("Calling this function on a transaction with a normal signature is not permitted!");
+                },
+                Some(Signature::MultiSig(ref sig)) => {
+                    sig.verify(&message, required_keys, pkeys)
+                },
+                None => {
+                    false
+                }
+            }
+        }
+    }
+
+    /// Verifies the multi signature of the transaction.
+    ///
+    /// Returns `false` if the signature field is missing.
+    pub fn verify_multi_sig_shares(&mut self, required_percentile: u8, share_map: ShareMap) -> bool {
+        let message = assemble_sign_message(&self);
+
+        match self.signature {
+            Some(Signature::Normal(_)) => { 
+                panic!("Calling this function on a transaction with a normal signature is not permitted!");
+            },
+            Some(Signature::MultiSig(ref sig)) => {
+                sig.verify_shares(&message, required_percentile, share_map)
+            },
+            None => {
+                false
+            }
+        }
+    }
 
     /// Serializes the transaction struct to a binary format.
     ///
@@ -236,6 +356,51 @@ impl OpenContract {
 
         Ok(open_contract)
     }
+
+    impl_hash!();
+}
+
+fn assemble_hash_message(obj: &OpenContract) -> Vec<u8> {
+    let mut signature = if let Some(ref sig) = obj.signature {
+        sig.to_bytes()
+    } else {
+        panic!("Signature field is missing!");
+    };
+
+    let mut buf: Vec<u8> = Vec::new();
+    let mut owner = obj.owner.to_bytes();
+    let fee_hash = &obj.fee_hash.0;
+    let code = obj.code.as_bytes();
+    let default_state = obj.default_state.as_bytes();
+    let mut fee = obj.fee.to_bytes();
+
+    // Compose data to hash
+    buf.append(&mut owner);
+    buf.append(&mut fee_hash.to_vec());
+    buf.append(&mut code.to_vec());
+    buf.append(&mut default_state.to_vec());
+    buf.append(&mut fee);
+    buf.append(&mut signature);
+
+    buf
+}
+
+fn assemble_sign_message(obj: &OpenContract) -> Vec<u8> {
+    let mut buf: Vec<u8> = Vec::new();
+    let mut owner = obj.owner.to_bytes();
+    let fee_hash = &obj.fee_hash.0;
+    let code = obj.code.as_bytes();
+    let default_state = obj.default_state.as_bytes();
+    let mut fee = obj.fee.to_bytes();
+
+    // Compose data to hash
+    buf.append(&mut owner);
+    buf.append(&mut fee_hash.to_vec());
+    buf.append(&mut code.to_vec());
+    buf.append(&mut default_state.to_vec());
+    buf.append(&mut fee);
+
+    buf
 }
 
 use quickcheck::Arbitrary;
@@ -257,10 +422,125 @@ impl Arbitrary for OpenContract {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use account::NormalAddress;
+    use crypto::Identity;
 
     quickcheck! {
         fn serialize_deserialize(tx: OpenContract) -> bool {
             tx == OpenContract::from_bytes(&OpenContract::to_bytes(&tx).unwrap()).unwrap()
+        }
+
+        fn verify_hash(tx: OpenContract) -> bool {
+            let mut tx = tx;
+
+            for _ in 0..3 {
+                tx.hash();
+            }
+
+            tx.verify_hash()
+        }
+
+        fn verify_signature(
+            code: String,
+            default_state: String,
+            fee: Balance,
+            fee_hash: Hash
+        ) -> bool {
+            let id = Identity::new();
+
+            let mut tx = OpenContract {
+                owner: Address::normal_from_pkey(*id.pkey()),
+                fee_hash: fee_hash,
+                fee: fee,
+                default_state: default_state,
+                code: code,
+                signature: None,
+                hash: None
+            };
+
+            tx.sign(id.skey().clone());
+            tx.verify_sig()
+        }
+
+        fn verify_multi_signature(
+            code: String,
+            default_state: String,
+            fee: Balance,
+            fee_hash: Hash
+        ) -> bool {
+            let mut ids: Vec<Identity> = (0..30)
+                .into_iter()
+                .map(|_| Identity::new())
+                .collect();
+
+            let creator_id = ids.pop().unwrap();
+            let pkeys: Vec<Pk> = ids
+                .iter()
+                .map(|i| *i.pkey())
+                .collect();
+
+            let mut tx = OpenContract {
+                owner: Address::multi_sig_from_pkeys(&pkeys, *creator_id.pkey(), 4314),
+                fee_hash: fee_hash,
+                fee: fee,
+                default_state: default_state,
+                code: code,
+                signature: None,
+                hash: None
+            };
+
+            // Sign using each identity
+            for id in ids {
+                tx.sign(id.skey().clone());
+            }
+            
+            tx.verify_multi_sig(10, &pkeys)
+        }
+
+        fn verify_multi_signature_shares(
+            code: String,
+            default_state: String,
+            fee: Balance,
+            fee_hash: Hash
+        ) -> bool {
+            let mut ids: Vec<Identity> = (0..30)
+                .into_iter()
+                .map(|_| Identity::new())
+                .collect();
+
+            let creator_id = ids.pop().unwrap();
+            let pkeys: Vec<Pk> = ids
+                .iter()
+                .map(|i| *i.pkey())
+                .collect();
+
+            let addresses: Vec<NormalAddress> = pkeys
+                .iter()
+                .map(|pk| NormalAddress::from_pkey(*pk))
+                .collect();
+            
+            let mut share_map = ShareMap::new(); 
+
+            for addr in addresses.clone() {
+                share_map.add_shareholder(addr, 100);
+            }
+
+            let mut tx = OpenContract {
+                owner: Address::shareholders_from_pkeys(&pkeys, *creator_id.pkey(), 4314),
+                fee_hash: fee_hash,
+                fee: fee,
+                default_state: default_state,
+                code: code,
+                signature: None,
+                hash: None
+            };
+
+            // Sign using each identity
+            for id in ids {
+                tx.sign(id.skey().clone());
+            }
+            
+            tx.verify_multi_sig_shares(10, share_map)
         }
     }
 }
