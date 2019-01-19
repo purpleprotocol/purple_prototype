027235c824a9aa211ad6037b506a594d170ba252
octavonce <octavian.oncescu@gmail.com>
2019-01-19 16:05:54 +0200

    Finished contract structure

--- src/purple_vm/src/code.rs
+++ src/purple_vm/src/code.rs
@@ -33,33 +33,44 @@ impl Code {
     /// The source code is a set of function signatures, each 
     /// of which has a representing block of instructions.
     ///   
-    ///
-    /// The structure of a contract is the following:
-    /// 1) Version       - 8bits              - The version of the instruction set
-    /// 2) Data          - Arbitrary length   - The data field. This field contains all of the contract sections.
-    ///
-    /// The data field is composed of 3 sections:
+    /// A contract is composed of 2 sections:
     /// 1) The imports section 
-    /// 2) The exports section
-    /// 3) The functions section
+    /// 2) The functions section
+    ///
+    /// Everything is encoded using Big Endian
+    ///
+    /// The binary structure of a contract is the following:
+    /// 1) Version               - 8bits                - The version of the instruction set.
+    /// 2) Imports length        - 16bits               - The length of the imports section.
+    /// 3) Functions length      - 16bits               - The length of the functions section.
+    /// 4) Imports payload       - Variable length      - The imports section data.
+    /// 5) Functions payload     - Variable length      - The functions section data.
     ///
     /// The imports section describes function imports from
     /// other contracts. It has the following format: 
-    /// 1) Addresses length   - 16bits 
-    /// 2) Data length        - 16bits
-    /// 3) Addresses payload  - Binary of addresses length
-    /// 4) Data payload       - Binary of data length
+    /// 1) Addresses length      - 16bits               - The length of the addresses field.
+    /// 2) Data length           - 16bits               - The length of the data field. 
+    /// 3) Addresses payload     - Variable length      - The addresses field. Contains all contract addresses that can be called.
+    /// 4) Data payload          - Variable length      - The imports section entries.
     ///
     /// An entry in the imports section has 
     /// the following format:
-    /// 1) Functon name length   - 8bits
-    /// 2) Address index         - 16bits  
-    /// 3) Functon name          - Binary of function name length
+    /// 1) Functon name length   - 8bits                - The length of the function name field. 
+    /// 2) Address index         - 16bits               - The index of the address from the address index space.
+    /// 3) Functon name          - Variable length      - The name of the function.
     ///
-    /// The exports section describes the functions exported
-    /// from the contract. It has the following format:
-    /// TODO: Add exports section definitions
-    /// TODO: Add functions section definitions
+    /// 
+    /// The functions section describes the functions that
+    /// are defined in the contract.
+    ///
+    /// An entry in the functions section has the following format:
+    /// 1) Function name length  - 8bits                - The length of the function name field.
+    /// 2) Arity                 - 8bits                - The arity of the function.
+    /// 3) Return type           - 8bits                - The return type of the function.
+    /// 4) Block length          - 16bits               - The length of the block field.
+    /// 5) Function name         - Variable length      - The name of the function. Must be valid utf8.
+    /// 6) Argument types        - Variable length      - The types of the arguments.
+    /// 7) Block                 - Variable length      - The function's block of code.
     pub fn validate(&self) -> bool {
         unimplemented!(); 
     }
