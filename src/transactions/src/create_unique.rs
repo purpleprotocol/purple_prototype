f20c336aa7bb47dbbe5a72115406d89fe672f57a
octavonce <octavian.oncescu@gmail.com>
2019-01-30 16:01:56 +0200

    Added CreateUnique hash and sign logic

--- src/transactions/src/create_unique.rs
+++ src/transactions/src/create_unique.rs
@@ -183,9 +183,424 @@ impl CreateUnique {
 }
 
 fn assemble_hash_message(obj: &CreateUnique) -> Vec<u8> {
-    unimplemented!();
+    let mut signature = if let Some(ref sig) = obj.signature {
+        sig.to_bytes()
+    } else {
+        panic!("Signature field is missing!");
+    };
+
+    let mut buf: Vec<u8> = Vec::new();
+    let mut creator = obj.creator.to_bytes();
+    let mut receiver = obj.receiver.to_bytes();
+    let mut name = obj.name;
+    let mut fee = obj.fee.to_bytes();
+    let asset_hash = obj.asset_hash.0;
+    let fee_hash = obj.fee_hash.0;
+
+    // Compose data to hash
+    buf.append(&mut creator);
+    buf.append(&mut receiver);
+    buf.append(&mut name.to_vec());
+
+    // Write meta if present
+    if let Some(meta) = obj.meta1 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta2 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta3 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta4 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta5 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    buf.append(&mut asset_hash.to_vec());
+    buf.append(&mut fee_hash.to_vec());
+    buf.append(&mut fee);
+    buf.append(&mut signature);
+
+    buf
 }
 
 fn assemble_sign_message(obj: &CreateUnique) -> Vec<u8> {
-    unimplemented!();
+    let mut buf: Vec<u8> = Vec::new();
+    let mut creator = obj.creator.to_bytes();
+    let mut receiver = obj.receiver.to_bytes();
+    let mut name = obj.name;
+    let mut fee = obj.fee.to_bytes();
+    let asset_hash = obj.asset_hash.0;
+    let fee_hash = obj.fee_hash.0;
+
+    // Compose data to hash
+    buf.append(&mut creator);
+    buf.append(&mut receiver);
+    buf.append(&mut name.to_vec());
+
+    // Write meta if present
+    if let Some(meta) = obj.meta1 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta2 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta3 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta4 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    if let Some(meta) = obj.meta5 {
+        buf.append(&mut meta.to_vec());
+    }
+
+    buf.append(&mut asset_hash.to_vec());
+    buf.append(&mut fee_hash.to_vec());
+    buf.append(&mut fee);
+
+    buf
+}
+
+use quickcheck::Arbitrary;
+use rand::Rng;
+
+#[derive(Clone, Debug)]
+struct Array32(pub [u8; 32]);
+
+impl Arbitrary for Array32 {
+    fn arbitrary<G : quickcheck::Gen>(g: &mut G) -> Array32 {
+        Array32(rand::thread_rng().gen::<[u8; 32]>())
+    }
+}
+
+impl Arbitrary for CreateUnique {
+    fn arbitrary<G : quickcheck::Gen>(g: &mut G) -> CreateUnique {
+        let name: Array32 = Arbitrary::arbitrary(g);
+        let meta1: Option<Array32> = Arbitrary::arbitrary(g);
+        let meta2: Option<Array32> = Arbitrary::arbitrary(g);
+        let meta3: Option<Array32> = Arbitrary::arbitrary(g);
+        let meta4: Option<Array32> = Arbitrary::arbitrary(g);
+        let meta5: Option<Array32> = Arbitrary::arbitrary(g);
+
+        let meta1 = if let Some(Array32(result)) = meta1 {
+            Some(result)
+        } else {
+            None
+        };
+
+        let meta2 = if let Some(Array32(result)) = meta2 {
+            Some(result)
+        } else {
+            None
+        };
+
+        let meta3 = if let Some(Array32(result)) = meta3 {
+            Some(result)
+        } else {
+            None
+        };
+
+        let meta4 = if let Some(Array32(result)) = meta4 {
+            Some(result)
+        } else {
+            None
+        };
+
+        let meta5 = if let Some(Array32(result)) = meta5 {
+            Some(result)
+        } else {
+            None
+        };
+
+        CreateUnique {
+            creator: Arbitrary::arbitrary(g),
+            receiver: Arbitrary::arbitrary(g),
+            asset_hash: Arbitrary::arbitrary(g),
+            name: name.0,
+            meta1: meta1,
+            meta2: meta2,
+            meta3: meta3,
+            meta4: meta4,
+            meta5: meta5,
+            fee_hash: Arbitrary::arbitrary(g),
+            fee: Arbitrary::arbitrary(g),
+            hash: Some(Arbitrary::arbitrary(g)),
+            signature: Some(Arbitrary::arbitrary(g)),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use account::NormalAddress;
+    use crypto::Identity;
+
+    quickcheck! {
+        // fn serialize_deserialize(tx: CreateUnique) -> bool {
+        //     tx == CreateUnique::from_bytes(&CreateUnique::to_bytes(&tx).unwrap()).unwrap()
+        // }
+
+        fn verify_hash(tx: CreateUnique) -> bool {
+            let mut tx = tx;
+
+            for _ in 0..3 {
+                tx.hash();
+            }
+
+            tx.verify_hash()
+        }
+
+        fn verify_signature(
+            receiver: Address,
+            fee: Balance, 
+            name: Array32,
+            meta: (Option<Array32>, Option<Array32>, Option<Array32>, Option<Array32>, Option<Array32>),
+            asset_hash: Hash, 
+            fee_hash: Hash
+        ) -> bool {
+            let id = Identity::new();
+            let (
+                meta1,
+                meta2,
+                meta3,
+                meta4,
+                meta5
+            ) = meta;
+
+            let meta1 = if let Some(Array32(result)) = meta1 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta2 = if let Some(Array32(result)) = meta2 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta3 = if let Some(Array32(result)) = meta3 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta4 = if let Some(Array32(result)) = meta4 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta5 = if let Some(Array32(result)) = meta5 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let mut tx = CreateUnique {
+                creator: Address::normal_from_pkey(*id.pkey()),
+                receiver: receiver,
+                name: name.0,
+                meta1: meta1,
+                meta2: meta2,
+                meta3: meta3,
+                meta4: meta4,
+                meta5: meta5,
+                fee: fee,
+                asset_hash: asset_hash,
+                fee_hash: fee_hash,
+                signature: None,
+                hash: None
+            };
+
+            tx.sign(id.skey().clone());
+            tx.verify_sig()
+        }
+
+        fn verify_multi_signature(
+            receiver: Address,
+            name: Array32,
+            meta: (Option<Array32>, Option<Array32>, Option<Array32>, Option<Array32>, Option<Array32>),
+            fee: Balance,
+            asset_hash: Hash,
+            fee_hash: Hash
+        ) -> bool {
+            let mut ids: Vec<Identity> = (0..30)
+                .into_iter()
+                .map(|_| Identity::new())
+                .collect();
+
+            let creator_id = ids.pop().unwrap();
+            let pkeys: Vec<Pk> = ids
+                .iter()
+                .map(|i| *i.pkey())
+                .collect();
+            
+            let (
+                meta1,
+                meta2,
+                meta3,
+                meta4,
+                meta5
+            ) = meta;
+
+            let meta1 = if let Some(Array32(result)) = meta1 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta2 = if let Some(Array32(result)) = meta2 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta3 = if let Some(Array32(result)) = meta3 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta4 = if let Some(Array32(result)) = meta4 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta5 = if let Some(Array32(result)) = meta5 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let mut tx = CreateUnique {
+                creator: Address::multi_sig_from_pkeys(&pkeys, *creator_id.pkey(), 4314),
+                receiver: receiver,
+                name: name.0,
+                meta1: meta1,
+                meta2: meta2,
+                meta3: meta3,
+                meta4: meta4,
+                meta5: meta5,
+                fee: fee,
+                asset_hash: asset_hash,
+                fee_hash: fee_hash,
+                signature: None,
+                hash: None
+            };
+
+            // Sign using each identity
+            for id in ids {
+                tx.sign(id.skey().clone());
+            }
+            
+            tx.verify_multi_sig(10, &pkeys)
+        }
+
+        fn verify_multi_signature_shares(
+            receiver: Address,
+            name: Array32,
+            meta: (Option<Array32>, Option<Array32>, Option<Array32>, Option<Array32>, Option<Array32>),
+            fee: Balance,
+            asset_hash: Hash,
+            fee_hash: Hash
+        ) -> bool {
+            let mut ids: Vec<Identity> = (0..30)
+                .into_iter()
+                .map(|_| Identity::new())
+                .collect();
+
+            let creator_id = ids.pop().unwrap();
+            let pkeys: Vec<Pk> = ids
+                .iter()
+                .map(|i| *i.pkey())
+                .collect();
+
+            let addresses: Vec<NormalAddress> = pkeys
+                .iter()
+                .map(|pk| NormalAddress::from_pkey(*pk))
+                .collect();
+            
+            let mut share_map = ShareMap::new(); 
+
+            for addr in addresses.clone() {
+                share_map.add_shareholder(addr, 100);
+            }
+
+            let (
+                meta1,
+                meta2,
+                meta3,
+                meta4,
+                meta5
+            ) = meta;
+
+            let meta1 = if let Some(Array32(result)) = meta1 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta2 = if let Some(Array32(result)) = meta2 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta3 = if let Some(Array32(result)) = meta3 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta4 = if let Some(Array32(result)) = meta4 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let meta5 = if let Some(Array32(result)) = meta5 {
+                Some(result)
+            } else {
+                None
+            };
+
+            let mut tx = CreateUnique {
+                creator: Address::shareholders_from_pkeys(&pkeys, *creator_id.pkey(), 4314),
+                receiver: receiver,
+                name: name.0,
+                meta1: meta1,
+                meta2: meta2,
+                meta3: meta3,
+                meta4: meta4,
+                meta5: meta5,
+                fee: fee,
+                asset_hash: asset_hash,
+                fee_hash: fee_hash,
+                signature: None,
+                hash: None
+            };
+
+            // Sign using each identity
+            for id in ids {
+                tx.sign(id.skey().clone());
+            }
+            
+            tx.verify_multi_sig_shares(10, share_map)
+        }
+    }
 }
