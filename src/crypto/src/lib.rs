08f29d607136209809480b2cb0aead058e8a2531
Octavian Oncescu <octavian.oncescu@gmail.com>
2019-06-09 21:15:14 +0200

    Added crypto seal/open api

--- src/crypto/src/lib.rs
+++ src/crypto/src/lib.rs
@@ -43,6 +43,8 @@ pub use rust_sodium::crypto::kx::{
     server_session_keys as server_sk,
 };
 pub use rust_sodium::crypto::sign::{gen_keypair, PublicKey, SecretKey};
+pub use rust_sodium::crypto::aead;
+pub use rust_sodium::crypto::aead::Nonce;
 pub use signature::*;
 
 mod blake_hasher;
@@ -58,6 +60,23 @@ pub fn sign(message: &[u8], skey: &SecretKey) -> Signature {
 
 pub fn verify(message: &[u8], signature: &Signature, pkey: &PublicKey) -> bool {
     verify_detached(&signature.inner(), message, pkey)
+}
+
+pub fn seal(message: &[u8], key: &KxPublicKey) -> (Vec<u8>, Nonce) {
+    let n = aead::gen_nonce();
+    let key = aead::Key::from_slice(&key.0).unwrap();
+    let ciphertext = aead::seal(message, None, &n, &key);
+    
+    (ciphertext, n)
+}
+
+pub fn open(ciphertext: &[u8], key: &KxPublicKey, nonce: &Nonce) -> Result<Vec<u8>, ()> {
+    let key = aead::Key::from_slice(&key.0).unwrap();
+    
+    match aead::open(ciphertext, None, nonce, &key) {
+        Ok(result) => Ok(result),
+        _ => Err(())
+    }
 }
 
 #[derive(Clone, Debug)]
