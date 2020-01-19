05d261bddc0d91257af97a3be46ce822fb75b0f2
Octavian Oncescu <octavian.oncescu@gmail.com>
2020-01-19 18:14:21 +0000

    Added RejectTx packet

--- src/network/src/common.rs
+++ src/network/src/common.rs
@@ -195,6 +195,11 @@ pub fn handle_packet<N: NetworkInterface>(
             _ => Err(NetworkErr::PacketParseErr),
         },
 
+        RejectTx::PACKET_TYPE => match RejectTx::from_bytes(packet) {
+            Ok(packet) => RejectTx::handle(network, peer_addr, &packet, conn_type),
+            _ => Err(NetworkErr::PacketParseErr),
+        },
+
         _ => {
             debug!("Could not parse packet with type {} from {}", packet_type, peer_addr);
             Err(NetworkErr::PacketParseErr)
